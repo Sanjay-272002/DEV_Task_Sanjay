cadc645bc76dca23a319ac199c3f3ef55034e4c0
Sanjay Kumar <sanjaykumaranna799@gmail.com>
2023-05-21 13:47:29 +0530

    feat: added proxy

--- frontend/src/core/helper/imageHelper.js
+++ frontend/src/core/helper/imageHelper.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export default function ImageHelper({product}) {
-    const imageurl=product ? product.image:`https://www.pexels.com/search/photoshop/`
+  console.log("images",product.image)
+    const imageurl=product.image ? product.image:`https://www.pexels.com/search/photoshop/`
   return (
     <div className='rounded border-success px-2'>
- <img src={imageurl}
+ <img src={' http://localhost:8000'+imageurl}
  style={{maxHeight:"100%",maxWidth:"100%"}}
  className='mb-3 rounded'
  alt=""/>    </div>
